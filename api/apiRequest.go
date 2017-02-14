50a74230052fa9c7f32b9dc571537fce400ec9c2
kext <luffyke@gmail.com>
2017-02-14 14:53:11 +0800

    omitempty for ApiRequest

--- api/apiRequest.go
+++ api/apiRequest.go
@@ -2,25 +2,25 @@ package api
 
 type ApiRequest struct {
 	Id     string                 `json:"id"`
-	Sign   string                 `json:"sign"`
-	Client Client                 `json:"client"`
-	Page   Page                   `json:"page"`
-	User   User                   `json:"user"`
+	Sign   string                 `json:"sign,omitempty"`
+	Client Client                 `json:"client,omitempty"`
+	Page   Page                   `json:"page,omitempty"`
+	User   User                   `json:"user,omitempty"`
 	Data   map[string]interface{} `json:"data"`
 }
 
 type Client struct {
-	Caller   string            `json:"caller"`
-	Os       string            `json:"os"`
-	Ver      string            `json:"ver"`
-	Platform string            `json:"platform"`
-	Ch       string            `json:"ch"`
-	Ex       map[string]string `json:"ex"`
+	Caller   string            `json:"caller,omitempty"`
+	Os       string            `json:"os,omitempty"`
+	Ver      string            `json:"ver,omitempty"`
+	Platform string            `json:"platform,omitempty"`
+	Ch       string            `json:"ch,omitempty"`
+	Ex       map[string]string `json:"ex,omitempty"`
 }
 
-func (this ApiRequest) CheckData(keys ...string) bool {
+func (c ApiRequest) CheckData(keys ...string) bool {
 	for _, key := range keys {
-		if _, ok := this.Data[key]; !ok {
+		if _, ok := c.Data[key]; !ok {
 			return false
 		}
 	}
